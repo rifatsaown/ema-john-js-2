349e91789e90d34334e7ead6fc5fd8fe29940706
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-11-28 22:36:53 +0600

    cart compute

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import "./Cart.css";
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
+  let total = 0;
+  let shipping = 0;
+  for (const product of cart) {
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  }
+  let tax = (total * 0.1).toFixed(2);
+
   return (
-    <div>
-      <h3>Order Summary</h3>
+    <div className="cart">
+      <h2>Order Summary</h2>
       <p>Select Item : {cart.length}</p>
-      <p>Total Price : $</p>
+      <p>Total Price : ${total}</p>
+      <p>Total Shipping : ${shipping}</p>
+      <p>Total Tax : ${tax}</p>
+      <h4>Grand Total : </h4>
     </div>
   );
 };
