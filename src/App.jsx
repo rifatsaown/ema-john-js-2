d40f52fa9a04d2bb3af81e87c5b0b8681cc9944a
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-12-04 18:40:38 +0600

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Header from "./components/Header/Header";
 import Shop from "./components/Shop/Shop";
 
-function App(){
+function App() {
   return (
     <div className="App">
-      <Header/>
-      <Shop/>
+      <Header />
+      <Shop />
     </div>
   );
 }
